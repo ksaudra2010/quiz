d4a21564334d251237f5d6ca84a9f2ff43aee51f
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-05-27 22:24:59 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -4,25 +4,21 @@ var questions = [
         choices: ["alerts", "numbers", "strings", "booleans"],
         answer: "alerts"
     },
-    
     {
         title: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
-    
     {
         title: "Javascript can change HTML attributes, values, elements and styles:",
         choices: ["True", "False"],
         answer: "True"
     },
-    
     {
         title: "Javascript can only be written in external files, NOT within the HTML file.",
         choices: ["True", "False"],
         answer: "False"
     },
-    
     {
         title: "Commonly used identifiers for storing data values DO NOT include:",
         choices: ["var", "const", "let", "allow"],
